62e8cd77ffc4b35af9b328ddf0c821a8863f827f
Francois-lenne <francois.lenne@hotmail.fr>
2024-10-09 14:12:42 +0200

    finish the pandas dataframe

--- src/data_transformation/transform.py
+++ src/data_transformation/transform.py
@@ -11,8 +11,15 @@ results = analyze_video_for_persons(video_path)
 # Convert the dictionary to a pandas DataFrame
 df = pd.DataFrame.from_dict(results, orient='index').reset_index()
 
-# Rename the columns
 df.columns = ['frame', 'person_present']
+
+
+df['day'] = video_path.split("_")[-3]
+
+df['hour'] = video_path.split("_")[-2] + "_" + video_path.split("_")[-1].split(".")[0]
+
+df['hour'] = df['hour'].str.replace('_', 'h')
+
 
 # Display the DataFrame
 print(df)
