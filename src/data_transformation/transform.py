ef2a436e7ff4241f9f013ace7bf5796599ad166f
Francois-lenne <francois.lenne@hotmail.fr>
2024-10-09 13:47:44 +0200

    transform the dict into  a pandas dataframe

--- src/data_transformation/transform.py
+++ src/data_transformation/transform.py
@@ -1,5 +1,5 @@
 from model import analyze_video_for_persons
-
+import pandas as pd
 # Path to your MP4 video
 video_path = "downloaded_video_2024-09-29_10_34.mp4"
 
@@ -8,3 +8,11 @@ results = analyze_video_for_persons(video_path)
 
 
 
+# Convert the dictionary to a pandas DataFrame
+df = pd.DataFrame.from_dict(results, orient='index').reset_index()
+
+# Rename the columns
+df.columns = ['frame', 'person_present']
+
+# Display the DataFrame
+print(df)
