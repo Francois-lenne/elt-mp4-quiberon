9013927fd3d7a1a5af477a40ab240186cfbf1873
Francois-lenne <francois.lenne@hotmail.fr>
2024-10-10 11:33:46 +0200

    debugging the insert in teh bronze table in bigquery

--- src/data_transformation/transform.py
+++ src/data_transformation/transform.py
@@ -3,20 +3,9 @@ import pandas as pd
 from google.cloud import bigquery
 from google.auth import default
 from google.cloud import storage
-from google.auth import default
-
-
-
-
-
 
 # Path to your MP4 video
 video_path = "downloaded_video_2024-09-29_10_34.mp4"
-
-
-
-
-
 
 # Analyze the video
 def retrieve_person_frame(video_path):
@@ -36,22 +25,15 @@ def retrieve_person_frame(video_path):
 
     df['hour'] = df['hour'].str.replace('_', 'h')
 
-
-
     return df
 
-
 df_video = retrieve_person_frame(video_path)
-
-
 
 # Get the project ID
 credentials, project_id = default()
 
-
 # Define your bucket name
-bucket_name = "your-bucket-name"
-
+bucket_name = "bucket_quiberon_video"
 
 # Retrieve all file names and paths from the bucket
 def list_files_in_bucket(bucket_name):
@@ -66,26 +48,21 @@ def list_files_in_bucket(bucket_name):
 
     return files
 
-
 # List all files in the bucket
 files = list_files_in_bucket(bucket_name)
 print(f"Total files: {len(files)}")
 
-
 # Load DataFrame into BigQuery
 def load_dataframe_to_bigquery(df, table_id):
     client = bigquery.Client()
-    job = client.insert_rows_from_dataframe(df, table_id)
+    job = client.load_table_from_dataframe(df, table_id)
     job.result()  # Wait for the job to complete
     print(f"Loaded {job.output_rows} rows into {table_id}.")
-
-
-
-
 
 # Define your BigQuery table ID
 table_id = f"{project_id}.video_quiberon.named_result_ml_bronze"
 
+print(f"Table ID: {table_id}")
 
 # Load DataFrame into BigQuery
 load_dataframe_to_bigquery(df_video, table_id)
